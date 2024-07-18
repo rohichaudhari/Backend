73719a859b2ca5d23ec36e538a4b640d1d2ac64f
Rohini <rohinichaudhari0307@gmail.com>
2024-07-18 09:55:34 +0530

    18/7/2024

---
+++ Mongodb/Lecture3.js
@@ -0,0 +1,34 @@
+// show dbs
+// use students
+// show collections
+db.data.insertMany([
+    {
+    "firstName":"Ragini",
+    "lastname":"chaudhari",
+    "age":25
+    },
+    {
+       "firstname":"kinjal",
+       "lastName":"chaudhari",
+       "hobbies":["dancing","cooking","reading"]
+    }
+        
+]);
+db.data.find()
+// use node
+// show collections
+db.book.find()
+db.book.find({"pages":{$eq:254}})
+db.userdata.find({})
+db.userdata.find({"name":{$eq:"Mrs. Dennis Schulist"}});
+db.userdata.find({"id":{$ne:3}});
+db.userdata.find({"id":{$gt:5}});
+db.userdata.find({"id":{$gte:5}});
+db.userdata.find({"id":{$lt:3}});
+db.userdata.find({"id":{$lte:3}});
+db.userdata.find({"id":{$in:[1,3,5]}});
+db.userdata.find({"id":{$nin:[1,3,5]}});
+
+
+
+
